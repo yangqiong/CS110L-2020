d6606037ef724663eac84dde8163be5dc2ea2168
yangqiong <yangqionggo@163.com>
2022-04-27 11:13:40 +0800

    feat: Part 2: A Generic LinkedList - Make LinkedList generic

--- week3/linked_list/src/main.rs
+++ week3/linked_list/src/main.rs
@@ -2,21 +2,12 @@ use linked_list::LinkedList;
 pub mod linked_list;
 
 fn main() {
-    let mut list: LinkedList<u32> = LinkedList::new();
-    assert!(list.is_empty());
-    assert_eq!(list.get_size(), 0);
-    for i in 1..12 {
-        list.push_front(i);
-    }
+    // Make LinkedList generic
+    let mut list: LinkedList<String> = LinkedList::new();
+    list.push_front("a".to_string());
+    list.push_front("b".to_string());
+    list.push_front("c".to_string());
+    list.push_front("d".to_string());
     println!("{}", list);
-    println!("list size: {}", list.get_size());
-    println!("top element: {}", list.pop_front().unwrap());
-    println!("{}", list);
-    println!("size: {}", list.get_size());
-    println!("{}", list.to_string()); // ToString impl for anything impl Display
-
-    // If you implement iterator trait:
-    //for val in &list {
-    //    println!("{}", val);
-    //}
+    assert_eq!(list.get_size(), 4);
 }
